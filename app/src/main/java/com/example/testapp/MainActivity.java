d7b0134fb56a9b566759f7180d1302fe03d9273a
NSF <mohammednaseefpn123@gmail.com>
2022-04-13 20:53:52 +0530

    commit1

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.testapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Thread obj = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(5000);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+
+                Intent i = new Intent(getApplicationContext(),Loginpage.class);
+                startActivity(i);
+            }
+        };
+        obj.start();
 
     }
 }
